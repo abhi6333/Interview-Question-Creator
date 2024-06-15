1283f32d1fbe8f0d447b1e16c4aa24aceb2b7cb3
abhi6333 <amishra6333@gamil.com>
2024-06-15 09:44:41 +0530

    setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Genrative AI Project",
+    version = "0.0.0",
+    author = "Abhishek",
+    author_email = "amishra6333@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
